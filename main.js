8cf9899fb3dcadea15f2ae20eed09d3dc70b044e
aliciaadj <adjeteyalicia@gmail.com>
2021-09-10 14:54:34 +0200

    first time using vue

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data: function () {
+        return {
+            product: 'Socks',
+            description: 'Socks are made out of fabric. You can put them on your feet. They keep you warm and comfortable'
+        }
+    }
+}
+)
